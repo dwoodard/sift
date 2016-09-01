96413fcd1f01001e9a6e4a674a9f2ae6bb39bcae
Dustin Woodard <dustinwoodard@weber.edu>
2016-09-01 15:36:06 -0600

    quick commit

---
+++ src/components/Sift.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="sift">Sift</div>
+</template>
+
+<script>
+    export default
+    {
+        data()
+        {
+            return {
+                msg: 'Hello Vue!'
+            }
+        }
+    }
+</script>
+
+<style>
+.sift{}
+</style>
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import Sift from './components/Sift.vue';
+
+export default Sift;
